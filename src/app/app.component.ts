709c8a0f69ff8b3cb5a1285cd530d0941be277a9
abrullc <adrianbrullcontreras@gmail.com>
2024-03-28 18:26:23 +0100

    Funcionalidad para abrir diálogos y creado formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { DataDisplayComponent } from './data-display/data-display.component';
+import { FormCreateComponent } from './form-create/form-create.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,26 @@ import { DataDisplayComponent } from './data-display/data-display.component';
     RouterOutlet,
     FormsModule,
     ReactiveFormsModule,
-    DataDisplayComponent
+    DataDisplayComponent,
+    FormCreateComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto-basico-angular';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(FormCreateComponent, {
+      width: '250px',
+      // You can add more dialog configuration options here
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+      // You can handle the form submission result here
+    });
+  }
 }
